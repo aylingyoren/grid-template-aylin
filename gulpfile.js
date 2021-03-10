0e80f2f12c68dd674fb8c470c37aea5a5a9ef93a
aylingyoren <aylingyoren21@gmail.com>
2021-03-10 23:36:56 +0300

    prepros_start
    work with prepros, start

--- gulpfile.js
+++ gulpfile.js
@@ -76,7 +76,6 @@ function watchFiles() {
   watch(path.scssFiles, series(scss));
   watch(path.htmlFiles, sync);
   watch(path.jsFiles, sync);
-  /* watch(path.cssFiles, sync); */
 }
 
 task('comb', series(comb));
