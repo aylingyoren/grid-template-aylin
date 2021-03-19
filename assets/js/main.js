a2555a63d3ddef1dc9dd2b75c0587498a1e3d30e
aylingyoren <aylingyoren21@gmail.com>
2021-03-19 10:55:19 +0300

    prepos_finish
    header n skills in scss, added responsive n hamburger menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,15 @@
 
 $(document).ready(function () {
 
-  $('.header-navbar__hamburger').on('click', function () {
+  $('.header-navbar__btn').on('click', function () {
     $(this).toggleClass('active');
-    $('.header-navbar__list').toggleClass('active');
+    $('.header-navbar__list').stop(true, true).slideToggle(500);
   });
 
+});
+
+$(document).ready(function(){
+	$('.header-navbar__btn').click(function(){
+		$(this).toggleClass('active');
+	});
 });
